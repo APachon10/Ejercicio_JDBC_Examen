b7b67ab1b9895fb0b5f881bc00b897293bd9bbe1
Alberto <Pachondestroy95@gmail.com>
2020-01-14 19:38:09 +0100

    Clase Conexion Creada

--- src/main/java/GestionDB/Conexion.java
+++ src/main/java/GestionDB/Conexion.java
@@ -1,10 +1,19 @@
 package GestionDB;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 import Interfaces.ParametrosConexion;
 
 public class Conexion implements ParametrosConexion{
-	
-	public void Conexion() {
-		
+	public Connection conexion (String url,String user,String password) {
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection(ParametrosConexion.url,ParametrosConexion.user,ParametrosConexion.password);
+			System.out.println("Conectado con exito ");
+		} catch (Exception e) {
+
+		}
+		return conn;
 	}
 }
